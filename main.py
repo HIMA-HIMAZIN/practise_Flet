a84ecd6305a9fa2f76fc942de611fe6bfd3c0356
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-11-23 17:10:10 +0900

    画面の追加

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from models.todo import Todo
 from screens.home_screen import home_screen
 from screens.add_todo_screen import add_todo_screen
 from screens.details_screen import details_screen
+from screens.edit_todo_screen import edit_todo_screen
 
 def main(page: ft.Page):
     page.title = "Todoアプリ"
@@ -10,23 +11,32 @@ def main(page: ft.Page):
 
     todos = []
 
-    # 各画面への遷移
     def show_home_screen():
-        page.controls.clear()  # ページをクリアして再描画
-        home_screen(page, todos, show_add_todo_screen, show_details_screen)
-        page.update()
+        home_screen(page, todos, show_add_todo_screen, show_details_screen, toggle_todo_status, edit_todo, delete_todo)
 
     def show_add_todo_screen():
-        page.controls.clear()  # ページをクリアして再描画
         add_todo_screen(page, todos, show_home_screen)
-        page.update()
 
     def show_details_screen(index):
-        page.controls.clear()  # ページをクリアして再描画
-        details_screen(page, todos[index], show_home_screen)
-        page.update()
+        details_screen(page, todos[index], toggle_todo_status, show_home_screen)
 
-    # 初期画面を表示
+    def toggle_todo_status(todo):
+        todo.status = not todo.status
+
+    def edit_todo(index):
+        todos[index].title = "編集されたタイトル"  # サンプル編集
+        show_home_screen()
+
+    def delete_todo(index):
+        todos.pop(index)
+        show_home_screen()
+
+    def edit_todo(index):
+        def update_todo():
+            todos[index] = todos[index]
+        edit_todo_screen(page, todos[index], update_todo, show_home_screen)
+
+
     show_home_screen()
 
 ft.app(target=main)
