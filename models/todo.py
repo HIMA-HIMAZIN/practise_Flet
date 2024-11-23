14cb02c60df432e81c3d2d20453b3d4e2cf2588b
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-11-23 17:10:19 +0900

    statusの追加

--- models/todo.py
+++ models/todo.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
 class Todo:
-    def __init__(self, title: str, details: str, created_at: str = None):
+    def __init__(self, title: str, details: str, status: bool = False, created_at: str = None):
         self.title = title
         self.details = details
+        self.status = status
         self.created_at = created_at or datetime.now().strftime("%Y-%m-%d %H:%M:%S")
