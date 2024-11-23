02302f52038fd8f4b424d3992a4e9f02b3670cab
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-11-23 17:09:58 +0900

    使用色の定義

---
+++ constants.py
@@ -0,0 +1,16 @@
+COLORS = {
+    "black": "#000000",
+    "red": "#FF3B30",
+    "blue": "#00BBFF",
+    "green": "#22C24A",
+    "dark_gray": "#252525",
+    "white": "#FFFFFF",
+    "yellow": "#FFD015",
+    "light_gray": "#F2F2F2",
+}
+
+FONT_STYLE = {
+    "headline": {"size": 24, "weight": "bold", "color": COLORS["black"]},
+    "subtitle": {"size": 18, "weight": "normal", "color": COLORS["dark_gray"]},
+    "body": {"size": 16, "weight": "normal", "color": COLORS["black"]},
+}
